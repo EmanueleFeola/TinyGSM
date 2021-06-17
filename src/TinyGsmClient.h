e1b72160128c0e73cf6f70d71be283f68b81f15e
EmanueleFeola <efeola99@gmail.com>
2021-06-17 10:37:17 +0200

    added GsmClientSecureSIM7600

--- src/TinyGsmClient.h
+++ src/TinyGsmClient.h
@@ -30,19 +30,7 @@ typedef TinyGsmSim800::GsmClientSim800 TinyGsmClient;
 #include "TinyGsmClientSIM7000.h"
 typedef TinyGsmSim7000                   TinyGsm;
 typedef TinyGsmSim7000::GsmClientSim7000 TinyGsmClient;
-
-#elif defined(TINY_GSM_MODEM_SIM7000SSL)
-#include "TinyGsmClientSIM7000SSL.h"
-typedef TinyGsmSim7000SSL                            TinyGsm;
-typedef TinyGsmSim7000SSL::GsmClientSim7000SSL       TinyGsmClient;
-typedef TinyGsmSim7000SSL::GsmClientSecureSIM7000SSL TinyGsmClientSecure;
-
-#elif defined(TINY_GSM_MODEM_SIM7070) || defined(TINY_GSM_MODEM_SIM7080) || \
-    defined(TINY_GSM_MODEM_SIM7090)
-#include "TinyGsmClientSIM7080.h"
-typedef TinyGsmSim7080                         TinyGsm;
-typedef TinyGsmSim7080::GsmClientSim7080       TinyGsmClient;
-typedef TinyGsmSim7080::GsmClientSecureSIM7080 TinyGsmClientSecure;
+typedef TinyGsmSim7000::GsmClientSecureSIM7000 TinyGsmClientSecure;
 
 #elif defined(TINY_GSM_MODEM_SIM5320) || defined(TINY_GSM_MODEM_SIM5360) || \
     defined(TINY_GSM_MODEM_SIM5300) || defined(TINY_GSM_MODEM_SIM7100)
@@ -55,6 +43,7 @@ typedef TinyGsmSim5360::GsmClientSim5360 TinyGsmClient;
 #include "TinyGsmClientSIM7600.h"
 typedef TinyGsmSim7600                   TinyGsm;
 typedef TinyGsmSim7600::GsmClientSim7600 TinyGsmClient;
+typedef TinyGsmSim7600::GsmClientSecureSIM7600 TinyGsmClientSecure;
 
 #elif defined(TINY_GSM_MODEM_UBLOX)
 #include "TinyGsmClientUBLOX.h"
